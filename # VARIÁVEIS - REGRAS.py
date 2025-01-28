05e42fc33116ea5f59436e9da1d31496e58b0d02
VyPython <vms.pereira@sapo.pt>
2025-01-28 23:08:24 +0000

    ddd

--- # VARIÁVEIS - REGRAS.py
+++ # VARIÁVEIS - REGRAS.py
@@ -1,5 +1,5 @@
 # VARIÁVEIS - REGRAS
-x = 10   # x é uma variável do tipo inteiro
-y = 2.5  # y é uma variável do tipo float
-name = "Vitor"  # name é uma variável do tipo string
+x = 10           # x é uma variável do tipo inteiro
+y = 2.5          # y é uma variável do tipo float
+name = "Vitor"   # name é uma variável do tipo string
 is_cooll = True  # is_cooll é uma variável do tipo boolean

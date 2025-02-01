244ee940a2d34834a5ec62c9c0e26366b9d54ae6
VyPython <vms.pereira@sapo.pt>
2025-02-01 18:55:21 +0000

    FT01_exec4

---
+++ UFCD10793_FT01/FT01_exerc4.py
@@ -0,0 +1,5 @@
+r=input("insere o raio da circunferência:\n")  #valor a inserir pelo utilizador
+r=int(r)                                       #conversão do valor inserido para inteiro
+PI=3.14159                                     #valor de pi
+V=(4/3*PI*r**3)                                #cálculo do volume da esfera com base no valor do raio inserido
+print("O volume da esfera de raio,",r, "é",V)  #impressão do resultado

4e5ce5cc2a1c7352e0a212dee782ef768749a9ed
VyPython <vms.pereira@sapo.pt>
2025-02-03 20:57:23 +0000

    sync exerc6

---
+++ UFCD10793_FT01/FT01_exerc6.py
@@ -0,0 +1,9 @@
+h=input("inserir a hora: ")
+m=input("inserir os minutos: ")
+s=input("inserir os segundos: ")
+h=int(h)
+m=int(m)
+s=int(s)
+t=((h*3600)+(m*60)+s)
+print("O total de segundos é:",t)
+5

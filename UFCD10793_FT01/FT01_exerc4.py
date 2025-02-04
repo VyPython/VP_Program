472bc0dae9f5b6a34c4d67d4d4aaca97667c105b
VyPython <vms.pereira@sapo.pt>
2025-02-04 20:56:17 +0000

    sync exe5

--- UFCD10793_FT01/FT01_exerc4.py
+++ UFCD10793_FT01/FT01_exerc4.py
@@ -1,5 +1,14 @@
 r=input("insere o raio da circunferência:\n")  #valor a inserir pelo utilizador
 r=int(r)                                       #conversão do valor inserido para inteiro
 PI=3.14159                                     #valor de pi
-V=(4/3*PI*r**3)                                #cálculo do volume da esfera com base no valor do raio inserido
+V= round (4/3*PI*r**3,2)                                #cálculo do volume da esfera com base no valor do raio inserido, round para arredondar o resultado
 print("O volume da esfera de raio,",r, "é",V)  #impressão do resultado
+''' Escreve um programa que calcule o volume de uma esfera. O valor do raio deverá ser 
+introduzido pelo utilizador (deverá ser criado o ficheiro ex04.py).
+import math
+pi = math.pi
+raio = input ("Introduza um valor para o raio:\n---->\t")
+raio = float(raio)
+volume = round (4/3*pi*raio**3, 2)
+output = f"o volume da esfera com raio {raio} é {volume}" #formated stringd
+print (output)'''
--- UFCD10793_FT01/FT01_exerc5.py
+++ UFCD10793_FT01/FT01_exerc5.py
@@ -2,4 +2,4 @@ import math
 a=5
 b=6
 h=math.sqrt(a**2+b**2)
-print("A hipotenusa do triângulo retângulo de catetos",a,"e",b,"é",h)
+print("A hipotenusa do triângulo retângulo de catetos",a,"e",b,"é",round(h,2))

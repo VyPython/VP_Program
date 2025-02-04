9dc5dd26166ec3a34797e6bb98fef46d520e84ad
VyPython <vms.pereira@sapo.pt>
2025-02-04 22:45:36 +0000

    sync

--- UFCD10793_FT02/FT03_exerc1.py
+++ UFCD10793_FT02/FT03_exerc1.py
@@ -4,12 +4,19 @@ if int(a) > 20:
     print(a/2)
 else:
     print ("o valor de a é menor que 20")'''
+    
     '''Escreve um programa que solicite um número inteiro ao utilizador e caso o mesmo seja maior que 20, 
 devolva o resultado da sua divisão por 2.'''
+
 num = int(input("Digite um número: "))
 if num > 20:
-    print(f"O resultado da divisão de {num} por 2 é {num / 2}")
+        print(f"O resultado da divisão de {num} por 2 é {num / 2}")
 else:
-    print("O número digitado é menor ou igual a 20")
+        print("O número digitado é menor ou igual a 20")
 
-
+numero1 = float(input("Número:"))
+numero2 = float(input("Número:"))
+if numero1 == numero2:
+    print("Os números são iguais")
+else:
+    print("Os números são diferentes")

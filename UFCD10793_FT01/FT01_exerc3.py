2f4387b37becde7844e97e9f87a7ec29c04e7a4a
VyPython <vms.pereira@sapo.pt>
2025-02-04 20:23:02 +0000

    sync exe1

--- UFCD10793_FT01/FT01_exerc3.py
+++ UFCD10793_FT01/FT01_exerc3.py
@@ -1,4 +1,10 @@
-v=input("insira um número:\n\t\v")
+v=input("insira um número:\n\t\v------->")
 v=int(v)
 db=2*v
 print("\vO dobro de",v,"é",db,"\v")
+# output = "o dobro de str (v) \n  é str (db) \n"
+# output = "o dobro de %d é %d" %(v,db)
+# output %f para float
+# output %s para string
+# output2 = "o dobro de {} é {}" .format(v,db)
+# output3 = f"o dobro de {v} é {db}"

7d7fffeaf27aed839b5e929fe7b14a64430fbebe
VyPython <vms.pereira@sapo.pt>
2025-02-11 20:00:58 +0000

    sync 11.2

--- FT04/FT04_exerc1.py
+++ FT04/FT04_exerc1.py
@@ -1,7 +1,7 @@
 '''Faz um programa que escreva o nome do mês que é introduzido, pelo utilizador, na
 forma numérica.'''
 
-mês=int(input("Introduza o número do mês:"))
+mês=int(input("Introduza o número do mês pretendido:"))
 match mês:
     case 1:
         print("janeiro")
--- FT04/FT04_exerc2.py
+++ FT04/FT04_exerc2.py
@@ -11,3 +11,16 @@ match estado:
         print("O estado cívil é: Viúvo(a)")
     case _:
         print("Estado cívil desconhecido")
+        
+        '''estadocivil = str(input("Digite o seu estado civil:"))
+match estadocivil.upper():
+    case "S":
+        print("Solteiro")
+    
+    case "C":
+        print("Casado")
+    case "V":
+        print("Viúvo")
+    
+    case _:
+        print("Estado civil desconhecido")'''
